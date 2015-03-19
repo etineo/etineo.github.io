6ac619dbf109efa15bb584edfeb10ccd6f982335
etineo <etineo@kippnyccp.org>
2015-03-19 13:19:31 -0400

    Update application.js

--- shoppinglist/application.js
+++ shoppinglist/application.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('#enter').on('click',function(){
     var item = $('input').val();
     $('ul').append('<li>'+item+"</li>");
+    });
   $('#empty').on('click',function(){
     var item = $('input').val();
     $('ul').empty('<li>'+item+"</li>");
