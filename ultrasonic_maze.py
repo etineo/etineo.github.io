9122419d5b413edcbf6908fa3047e766d4b975b5
etineo <etineo@kippnyccp.org>
2015-05-29 13:22:05 -0400

    Create ultrasonic_maze.py

---
+++ ultrasonic_maze.py
@@ -0,0 +1,23 @@
+from gopigo import *
+from random import randint
+
+def turn_right():
+  enc_tgt(1,0,15)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+
+def turn_left():
+  enc_tgt(0,1,15)
+  time.sleep(.1)
+  left()
+  time.sleep(2)
+  
+while 1<2:
+  while us_dist(15) > 20:
+    fwd()
+  stop()
+  if randint(0,1) == 0:
+    turn_left()
+  else:
+    turn_right()
