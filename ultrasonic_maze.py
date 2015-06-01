6dbe5003b07f91eb21d9ff0ffe098a3d1a6ade48
etineo <etineo@kippnyccp.org>
2015-06-01 13:06:08 -0400

    Update ultrasonic_maze.py

--- ultrasonic_maze.py
+++ ultrasonic_maze.py
@@ -1,11 +1,14 @@
 from gopigo import *
 import time
 from random import randint
+left_distance = 0
+right_distance = 0
 
 distance_to_stop=20
 print "Press ENTER to Start"
 raw_input()
 fwd()
+
 
 def turn_right():
   enc_tgt(1,0,15)
@@ -24,13 +27,13 @@ while 1<2:
     print "Dist:", dist,'cm'
     fwd()
   stop()
-  if randint(0,1) == 0:
+  servo(0)
+  left_distance = us_dist(15)
+  servo(180)
+  right_distance = us_dist(15)
+  if left_distance > right_distance:
     turn_left()
-    if us_dist(15) > 20:
-      turn_right()
-      turn_right()
+    servo(90)
   else:
     turn_right()
-    if us_dist(15) > 20:
-      turn_left()
-      turn_left()
+    servo(90)
